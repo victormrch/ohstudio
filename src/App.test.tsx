a4c3fb55b1035643a70a28973e773a6e60b01191
Víctor Ramón <victormanuelchica@gmail.com>
2023-07-11 22:22:41 +0200

    Add eslint & prettier.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Hello React heading', () => {
+	render(<App />);
+	const headingElement = screen.getByRole('heading', {name: 'Hello React!'});
+	expect(headingElement).toBeInTheDocument();
 });
