ea96b40b910710978b6b0f1783dfe8dfcda913d4
Víctor Ramón <victormanuelchica@gmail.com>
2023-07-14 22:10:20 +0200

    💅 Add style components and create GlobalStyle

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { AppRouter } from './core/router/router.component';
+import { AppRouter } from './core/router/Router';
+import { GlobalStyle } from './core/theme/app.style';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement,
 );
 root.render(
 	<React.StrictMode>
+		<GlobalStyle />
 		<AppRouter>
 			<App />
 		</AppRouter>
